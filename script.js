b38aeb08f682966981f59cf12e161bcb3179d143
pjflanagan <flanapet@umich.edu>
2015-01-17 18:05:26 -0500

    hopefully this is at least bearable

--- script.js
+++ script.js
@@ -12,67 +12,20 @@ window.onload = function(){
 				//Right point on logo + half of the holder width - burger_width
 	$("#burger").css({ "left":burger_left });
 	$("#title").css({"width":burger_left});
-
+	close_menu();
 
 }
 
-//add swiping up and down
-const MENU_HEIGHT = 387; //(height + padding)*7 + border-bottom + border radius <-remeasure
-var menu_top = -MENU_HEIGHT;
+
 var menuIsOpen = false;
 function show_menu(){
-	document.getElementById("menu").style.top = menu_top +"px";
-		//make this pixels and for the window height not doc %
-	if(menu_top >= -1){
-		document.getElementById("menu").style.top = 0 +"px";
-		return;
-	}
-	menu_top += 10;
-	play = setTimeout(show_menu,5);
-	menuIsOpen = true;
-	return;
+	document.getElementById("menu").style.display = "inline";
 }
 
 function close_menu(){
-	document.getElementById("menu").style.top = menu_top +"px";
-		//make this pixels and for the window height not doc %
-	if(menu_top <= -MENU_HEIGHT) return;
-	menu_top -= 10;
-	play = setTimeout(close_menu,5);
-	menuIsOpen = false;
-	return;
+	document.getElementById("menu").style.display = "none";
 }
 
-$("#menu").on("swipeup",function(){
-	close_menu();
-});
-
-document.addEventListener("scroll", function(){
-	var doc = document.getElementById("content");
-	var offset = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
-	if(menuIsOpen) close_menu();
-
-	//if(offset<300){ //125
-		//document.getElementById("slideshow").setAttribute("style","-webkit-filter:blur(" + (offset/30) + "px)");
-		//document.getElementById("slideshow").style.height = 200-offset +"px";
-		//document.getElementById("slide-title").style.opacity = 0;
-		//document.getElementById("logo").style.opacity = 1;
-
-		//document.getElementById("burger").style.position = "absolute";
-		//document.getElementById("burger").style.marginTop = "155px";
-
-	//}
-	//else{
-		//document.getElementById("slideshow").setAttribute("style","-webkit-filter:blur(" + (300/30) + "px)"); //(125/30) .setAttribute must go first
-		//document.getElementById("slideshow").style.height = 200-125 +"px";
-		//document.getElementById("slide-title").style.opacity = (offset-125)/200;
-		//document.getElementById("logo").style.opacity = 1-(offset-125)/75;
-
-		//BURGER
-		//document.getElementById("burger").style.position = "fixed";
-		//document.getElementById("burger").style.marginTop = "28px"; //marginTop-offset(125)
-	//}
-},false);
 
 function cycleImages(){
 	//z index 5-7
