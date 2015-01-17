a1f3b82d64a5d0257b228ab003a0cf52f7e0bcd5
pjflanagan <flanapet@umich.edu>
2015-01-17 17:50:04 -0500

    No blur, we're done with effects here
    :fire:

--- script.js
+++ script.js
@@ -52,8 +52,8 @@ document.addEventListener("scroll", function(){
 	var offset = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
 	if(menuIsOpen) close_menu();
 
-	if(offset<300){ //125
-		document.getElementById("slideshow").setAttribute("style","-webkit-filter:blur(" + (offset/30) + "px)");
+	//if(offset<300){ //125
+		//document.getElementById("slideshow").setAttribute("style","-webkit-filter:blur(" + (offset/30) + "px)");
 		//document.getElementById("slideshow").style.height = 200-offset +"px";
 		//document.getElementById("slide-title").style.opacity = 0;
 		//document.getElementById("logo").style.opacity = 1;
@@ -61,9 +61,9 @@ document.addEventListener("scroll", function(){
 		//document.getElementById("burger").style.position = "absolute";
 		//document.getElementById("burger").style.marginTop = "155px";
 
-	}
-	else{
-		document.getElementById("slideshow").setAttribute("style","-webkit-filter:blur(" + (300/30) + "px)"); //(125/30) .setAttribute must go first
+	//}
+	//else{
+		//document.getElementById("slideshow").setAttribute("style","-webkit-filter:blur(" + (300/30) + "px)"); //(125/30) .setAttribute must go first
 		//document.getElementById("slideshow").style.height = 200-125 +"px";
 		//document.getElementById("slide-title").style.opacity = (offset-125)/200;
 		//document.getElementById("logo").style.opacity = 1-(offset-125)/75;
@@ -71,18 +71,18 @@ document.addEventListener("scroll", function(){
 		//BURGER
 		//document.getElementById("burger").style.position = "fixed";
 		//document.getElementById("burger").style.marginTop = "28px"; //marginTop-offset(125)
-	}
+	//}
 },false);
 
 function cycleImages(){
 	//z index 5-7
-	var $active = $('#slideshow .active');
-		var $next = ($active.next().length > 0) ? $active.next() : $('#slideshow img:first');
-    $next.css('z-index',6);//move the next image up the pile
-    $active.fadeOut(2000,function(){//fade out the top image
-		$active.css('z-index',5).show().removeClass('active');
+	var active = $('#slideshow .active');
+		var next = (active.next().length > 0) ? active.next() : $('#slideshow img:first');
+    next.css('z-index',6);//move the next image up the pile
+    active.fadeOut(2000,function(){//fade out the top image
+		active.css('z-index',5).show().removeClass('active');
 		//reset the z-index and unhide the image
-    	$next.css('z-index',7).addClass('active');
+    	next.css('z-index',7).addClass('active');
     	//make the next image the top one
 		});
 }
