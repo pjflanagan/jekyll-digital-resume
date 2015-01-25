269d4357821149fdc2a36503527813fd3ade3599
pjflanagan <flanapet@umich.edu>
2015-01-24 22:32:23 -0500

    On my way to having pop ups
    Doesn't work yet but if it does the way I intend it to then it will be
    easy to create more unique pop up events :heart:

--- MRun/script.js
+++ MRun/script.js
@@ -106,69 +106,33 @@ window.onload = function(){
     	}
     });
 
+    function popup(){
+    	actions = false;
+    	$("#popup-back").css({
+    		//"background":"rgba(0,0,0,.5)"
+    		"display":"inline"
+    	},500);
+    	$("#popup").css({
+    		"display":"inline"
+    	});
+    }
 
-	/*function wheel(e) {
-		preventDefault(e);
-	}
+    function close_popup(){
+    	actions = true;
+    	$("#popup-back").css({
+    		//"background":"rgba(0,0,0,.5)"
+    		"display":"none"
+    	},500);
+    	$("#popup").css({
+    		"display":"none"
+    	});
+    }
 
-	function disable_scroll() {
-		if (window.addEventListener) {
-	    	window.addEventListener('DOMMouseScroll', wheel, false);
-		}
-		window.onmousewheel = document.onmousewheel = wheel;
-		document.onkeydown = keydown;
-	}*/
+    $("#faq").click(function(){
+    	$("#pop-content").load("faq.html", popup); 
+    });
 
-
-
-
-
-
-
-
-
-
-	/*
-	$(document).ready(function() {
-	    loadLatestTweet(1, "mrun");
-	});
-	String.prototype.parseURL = function() {
-	    return this.replace(/[A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&~\?\/.=]+/g, function(url) {
-	        return url.link(url);
-	    });
-	};
-	String.prototype.parseUsername = function() {
-	    return this.replace(/[@]+[A-Za-z0-9-_]+/g, function(u) {
-	        var username = u.replace("@","")
-	        return u.link("http://twitter.com/"+username);
-	    });
-	};
-	String.prototype.parseHashtag = function() {
-	    return this.replace(/[#]+[A-Za-z0-9-_]+/g, function(t) {
-	        var tag = t.replace("#","%23")
-	        return t.link("http://search.twitter.com/search?q="+tag);
-	    });
-	};
-	function parseDate(str) {
-	    var v=str.split(' ');
-	    return new Date(Date.parse(v[1]+" "+v[2]+", "+v[5]+" "+v[3]+" UTC"));
-	}
-	 
-	function loadLatestTweet(numTweets, un){
-	    var _url = 'https://api.twitter.com/1/statuses/user_timeline/' + un + '.json?callback=?&count='+numTweets+'&include_rts=1';
-	    $.getJSON(_url,function(data){
-	    for(var i = 0; i< data.length; i++){
-	            var tweet = data[i].text;
-	            var created = parseDate(data[i].created_at);
-	            var createdDate = created.getDate()+'-'+(created.getMonth()+1)+'-'+created.getFullYear()+' at '+created.getHours()+':'+created.getMinutes();
-	            //Uncomment below line to see the user Image
-				//tweet = "<img src='"+data[i].user.profile_image_url+"' />";
-				tweet = tweet.parseURL().parseUsername().parseHashtag();
-	            //Uncomment below line to displ tweet date.
-				//tweet += '<div class="tweeter-info"><p class="right">'+createdDate+'</p></div>'
-	            $("#twitter-feed").append('<p>'+tweet+'</p>');
-	        }
-	    });
-	}
-	*/
+    $("#popup-back").click(function(){
+    	close_popup();
+    });
 }
