01e86e4fced1393a7ec33118dbc51defd094e5ac
Peter Flanagan <pflanagan@freewheel.com>
2020-11-18 13:53:05 +0000

    add record and album

--- script.js
+++ script.js
@@ -10,46 +10,31 @@ window.onload = function() {
   G.SCROLL_END_Y = $("#content").offset().top;
   G.CONTENT_START_Y = G.SCROLL_END_Y - G.canvas.H;
 
-  // initialize scroll point (in case refresh starts us lower on the page)
-  const scroll = $(window).scrollTop();
-  G.scroll(scroll);
-  G.canvas.onScroll(scroll);
-
   // events
   $("#scroll-down").click(() => {
     $('html,body').animate({
       scrollTop: G.SCROLL_END_Y
-    }, 1200);
+    }, 1800);
   });
   
-  $(document).scroll(() => {
-    const scroll = $(window).scrollTop();
+  // initialize scroll point (in case refresh starts us lower on the page)
+  const scroll = $(window).scrollTop();
+  G.canvas.onScroll(scroll);
+  if($('#pix').width() >= 576) {
     G.scroll(scroll);
-  });
+    $(document).scroll(() => {
+      const scroll = $(window).scrollTop();
+      G.scroll(scroll);
+    });
+  }
+
 }
 
 G.scroll = (scroll) => {
   // scoll the content
-
-  // $('#pix').css({top: - 0.1 * scroll});
   if(scroll > G.CONTENT_START_Y) {
     $('#pix').css({top: - 0.1 * (scroll - G.CONTENT_START_Y)});
   } else {
     $('#pix').css({top: 0});
   }
-
-  // scroll the name logo
-  // const headerIntersectsNameY = G.CONTENT_START_Y + G.canvas.H / 2 - 30;
-  // if(scroll > headerIntersectsNameY) {
-  //   const intersectionAmount = scroll - headerIntersectsNameY;
-  //   $('.name-cover').css({
-  //     height: `${intersectionAmount}px`,
-  //     // marginTop: `${30-intersectionAmount}px`
-  //   })
-  // } else {
-  //   $('.name-cover').css({
-  //     height: '0px',
-  //     // marginTop: '30px'
-  //   })
-  // }
 }
