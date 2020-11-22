720afbb7dce0835c675617d8c18795014376651f
Peter Flanagan <pflanagan@freewheel.com>
2020-11-22 18:29:53 +0000

    experience starter

--- script.js
+++ script.js
@@ -7,35 +7,39 @@ window.onload = function() {
   // initialize global variables
   // TODO: canvas should take a scrollEnd point so it knows the ratio
   G.canvas = new Canvas();
-  G.SCROLL_END_Y = $("#content").offset().top;
+  G.SCROLL_END_Y = $('#content').offset().top;
   G.CONTENT_START_Y = G.SCROLL_END_Y - G.canvas.H;
 
   // events
-  $("#scroll-down").click(() => {
+  $('#scroll-down').click(() => {
     $('html,body').animate({
       scrollTop: G.SCROLL_END_Y
     }, 1800);
   });
+
+  // $('.slide-personal .photo-link').hover(() => {
+  //   console.log($(this).text());
+  // });
   
   // initialize scroll point (in case refresh starts us lower on the page)
   const scroll = $(window).scrollTop();
   G.canvas.onScroll(scroll);
-  if($('#pix').width() >= 576) {
-    G.scroll(scroll);
-    $(document).scroll(() => {
-      const scroll = $(window).scrollTop();
-      G.scroll(scroll);
-    });
-  }
+  // if($('#pix').width() >= 576) {
+  //   G.scroll(scroll);
+  //   $(document).scroll(() => {
+  //     const scroll = $(window).scrollTop();
+  //     G.scroll(scroll);
+  //   });
+  // }
 
 }
 
-G.scroll = (scroll) => {
-  // scoll the content
-  $("#splash-title").css({transform: `translateY(${- G.canvas.H / 4 * (scroll/G.CONTENT_START_Y)}px)`})
-  if(scroll > G.CONTENT_START_Y) {
-    $('#pix').css({transform: `translateY(${- 0.1 * (scroll - G.CONTENT_START_Y)}px)`});
-  } else {
-    $('#pix').css({transform: `translateY(0px)`});
-  }
-}
+// G.scroll = (scroll) => {
+//   // scoll the content
+//   $('#splash-title').css({transform: `translateY(${- G.canvas.H / 4 * (scroll/G.CONTENT_START_Y)}px)`})
+//   if(scroll > G.CONTENT_START_Y) {
+//     $('#pix').css({transform: `translateY(${- 0.1 * (scroll - G.CONTENT_START_Y)}px)`});
+//   } else {
+//     $('#pix').css({transform: `translateY(0px)`});
+//   }
+// }
